e62a1b8371cdd82389c29d3853138aacdb6f2788
hbseog <lllhades@naver.com>
2023-05-01 16:28:29 +0900

    navercloud bitcamp

--- src/chap23_jdbc/_01_jdbc_select.java
+++ src/chap23_jdbc/_01_jdbc_select.java
@@ -9,7 +9,7 @@ public class _01_jdbc_select {
 
         //접속할 데이터 베이스 url지정
         final String DB_URL = "jdbc:oracle:thin:@localhost:1521:XE";
-        
+
         //접속할 계정 정보
         final String USER = "c##study";
         final String PASSWORD = "1234";
@@ -20,33 +20,53 @@ public class _01_jdbc_select {
         try {
             //jdbc 드라이버 클래스 로드
             Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
-        //sql 쿼리 생성
-        statement = conn.createStatement();
-        String sql = "SELECT * FROM STUDENT";
-        
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            //sql 쿼리 생성
+            statement = conn.createStatement();
+            //학생번호, 이름, 평균 기말고사 성적
+            String sql =
+                    "SELECT SNO, SNAME, AVG(RESULT) AS AV " +
+                    "FROM STUDENT " +
+                    "NATURAL JOIN SCORE " +
+                    "GROUP BY SNO,SNAME";
+
             //결과를 담아줄 ResultSet 변수 선언
             //select -> executeQuery(sql);
             //insert, update, delete -> executeUpdate(sql);
             ResultSet resultSet = statement.executeQuery(sql);
-            
+
             //결과 출력
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 String sno = resultSet.getString("sno");
                 String sname = resultSet.getString("sname");
-                System.out.println("sno: " + sno + ", 이름: " + sname);
+                String av = resultSet.getString("AV");
+                System.out.println("sno: " + sno + ", 이름: " + sname + ", 평균 성적: " + av);
             }
             //사용된 객체 해제
             resultSet.close();
             statement.close();
             conn.close();
 
-        }catch(SQLException se){
-                System.out.println(se.getMessage());
-            }catch (Exception e){
+        } catch (SQLException se) {
+            System.out.println(se.getMessage());
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
-
+            //정상종료가 되지 않더라도 객체들은 해제해야 한다.
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException se) {
+                    System.out.println(se.getMessage());
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException se) {
+                    System.out.println(se.getMessage());
+                }
+            }
         }
     }
 }
